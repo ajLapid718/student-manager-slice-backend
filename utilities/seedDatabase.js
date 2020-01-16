c5f455c77d5e55db4b2fbac1ab0e825cf3fe8714
Allan James Lapid <ajLapid718@gmail.com>
2020-01-16 14:01:11 -0500

    set up eager loaded data with dummy data

--- utilities/seedDatabase.js
+++ utilities/seedDatabase.js
@@ -1,7 +1,11 @@
-const { Student } = require('../database/models');
+const { Student, Campus } = require('../database/models');
 
 const seedDatabase = async () => {
-  await Promise.all([
+  const HunterCollege = await Campus.create({
+    name: "Hunter College"
+  });
+
+  const students = await Promise.all([
     Student.create({
       firstName: "Kyrie",
       lastName: "Irving"
@@ -15,6 +19,10 @@ const seedDatabase = async () => {
       lastName: "Doncic"
     })
   ]);
+
+  for (let i = 0; i < students.length; i++) {
+    await students[i].setCampus(HunterCollege); // YOU CAN PASS IN EITHER THE ACTUAL INSTANCE OF THE MODEL AS AN ARGUMENT OR THE INTEGER ID OF THE MODEL THAT ALREADY EXISTS (KEEP THIS IN MIND WHEN WRITING OUT YOUR HANDLER FUNCTIONS) (YOU MIGHT HAVE A DROPDOWN OF CAMPUSES IN YOUR VIEW ASSOCIATED WITH AN ID SO THAT YOU CAN PASS THAT INFORMATION ACROSS TO THE BACKEND WHEN ESTABLISHING ASSOCIATIONS);
+  }
 }
 
 module.exports = seedDatabase;
